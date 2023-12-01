dfcd3d4fe0609fffefbfcb27ca04f95b41fbda0e
Kevin Dulche Laptop GMR Windows <kevdulche@gmail.com>
2023-12-01 15:53:27 -0600

    Conceptos basicos y contexto historico

---
+++ PLAYGROUNDS/01.sql
@@ -0,0 +1,21 @@
+-- En esta primera prueba de bases de datos vas a usar SQL, el lenguaje para hacer consultas en bases de datos relacionales.
+
+-- En el editor de código encuentras un primer ejemplo de la sentencia SELECT para imprimir datos en la consola.
+
+-- Tu desafío es replicar esa sentencia para imprimir los siguientes valores:
+
+-- 2 + 2 (la sentencia de prueba que ya encuentras en el editor)
+-- 18/3
+-- "Esto es una sentencia SELECT" (no olvides las comillas ")
+-- Escribe las 3 sentencias, imprime el resultado en la consola y corre las pruebas para comprobar que completaste el desafío.
+
+-- 💡 Recuerda que esta es la primera prueba. En las próximas clases aprenderás a profundidad cómo funciona SELECT, más sentencias SQL y otros tipos de bases de datos.
+
+
+-- Ejemplo de sentencia SQL con SELECT 👇
+SELECT (2+2);
+
+-- Escribe aquí las demás sentencias SQL 👇
+SELECT (18/3);
+
+SELECT ("Esto es una sentencia SELECT");
